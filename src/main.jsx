4d2ad0fd2d51302ed68a0c4eef6855065144739c
saikiranreddykarla <saikiranreddykarla7@gmail.com>
2025-08-31 23:34:19 +0530

    fix: update BrowserRouter basename and clean up vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/shopping-cart">
       <CartProvider>
         <App />
       </CartProvider>
